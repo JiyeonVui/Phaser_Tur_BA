c9fd01163df97ae647450fb0052afec0b791524d
manhnq <manh.nq0508@gmail.com>
2025-05-13 01:11:24 +0700

    add front end domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,6 +6,7 @@
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
   // ko can local host nua vi da luon cho phep moi truong dev
+  'https://trello-clone-pi-ten.vercel.app/'
 ]
 
 export const BOARD_TYPES = {
