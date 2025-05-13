7788aa8ffcd79ad5a5eb5ab6fd95bae729b78e25
manhnq <manh.nq0508@gmail.com>
2025-05-13 14:08:27 +0700

    add local host domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
 export const WHITELIST_DOMAINS = [
-  // 'http://localhost:5173'
+  'http://localhost:5173',
   // ko can local host nua vi da luon cho phep moi truong dev
   'https://trello-clone-git-master-manhnguyendevs-projects.vercel.app'
 ]
